1ac45e92955e8063322de1e1c82b261a3c0cf3d1
amrit-sbagga <amrit.sbagga@gmail.com>
2015-06-25 17:20:24 +0530

    Commented unused methods

--- app/src/main/java/com/amrit19/encryptiondecryptionexample/AESHelper.java
+++ app/src/main/java/com/amrit19/encryptiondecryptionexample/AESHelper.java
@@ -2,10 +2,7 @@ package com.amrit19.encryptiondecryptionexample;
 
 import android.util.Base64;
 
-import java.security.SecureRandom;
-
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -16,12 +13,13 @@ public class AESHelper {
 
 
     /**
-     * Encrypts the text.
+     * Encrypts the text
      *
+     * @param seed
      * @param clearText The text you want to encrypt
-     * @return Encrypted data if successful, or null if unsucessful
+     * @return Encrypted data if successful, or null if unsuccessful
      */
-    public static String encrypt1(String seed, String clearText) {
+    public static String encrypt(String seed, String clearText) {
         byte[] encryptedText = null;
         try {
             byte[] keyData = seed.getBytes();
@@ -31,6 +29,7 @@ public class AESHelper {
             encryptedText = c.doFinal(clearText.getBytes("UTF-8"));
             return Base64.encodeToString(encryptedText, Base64.DEFAULT);
         } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
     }
@@ -41,7 +40,7 @@ public class AESHelper {
      * @param encryptedText The text you want to encrypt
      * @return Decrypted data if successful, or null if unsucessful
      */
-    public static String decrypt1(String seed, String encryptedText) {
+    public static String decrypt(String seed, String encryptedText) {
         byte[] clearText = null;
         try {
             byte[] keyData = seed.getBytes();
@@ -51,11 +50,12 @@ public class AESHelper {
             clearText = c.doFinal(Base64.decode(encryptedText, Base64.DEFAULT));
             return new String(clearText, "UTF-8");
         } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
     }
 
-    public static String encrypt(String seed, String cleartext)
+   /* public static String encrypt(String seed, String cleartext)
             throws Exception {
         byte[] rawKey = getRawKey(seed.getBytes());
         byte[] result = encrypt(rawKey, cleartext.getBytes());
@@ -130,5 +130,5 @@ public class AESHelper {
         sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
     }
 
-
+*/
 }
